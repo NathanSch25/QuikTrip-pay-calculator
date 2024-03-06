9ec0bbdb7f7832816576eb50f83a93b34e8c0363
NathanSch25 <160279999+NathanSch25@users.noreply.github.com>
2024-03-06 08:55:01 -0600

    Update ra.py

--- RA/ra.py
+++ RA/ra.py
@@ -24,7 +24,7 @@ class RA:
         finalList = tempList[0][0], tempList[0][1], tempList[1][0], tempList[1][1], tempList[0][2]
         
         print(finalList)
-        O1 = Output(finalList[0], finalList[1], ((hours*52) / 12), 370, finalList[2], finalList[3], finalList[4])
+        O1 = Output(finalList[0], finalList[1], ((hours*52) / 12), 3, finalList[2], finalList[3], finalList[4])
         O1.Complete()
         
     def TBonusRA():
@@ -36,31 +36,31 @@ class RA:
             off = int(input(" how many days have you requested off this year? "))
         print('\n \n \n')
         if off <= 3:
-            TBonusAnual = 3087.92
-            TBonusQuarter = 385.99
+            TBonusAnual = 3
+            TBonusQuarter = 3
         elif off == 6:
-            TBonusAnual = 2779.14
-            TBonusQuarter = 289.50
+            TBonusAnual = 2
+            TBonusQuarter = 2
         elif off == 9:
-            TBonusAnual = 2593.86
-            TBonusQuarter = 192.99
+            TBonusAnual = 2
+            TBonusQuarter = 1
         elif off == 10:
-            TBonusAnual = 2408.58
+            TBonusAnual = 2
             
         elif off == 11:
-            TBonusAnual = 2254.19
+            TBonusAnual = 2
             
         elif off == 12:
-            TBonusAnual = 2068.91
+            TBonusAnual = 2
             
         elif off == 13:
-            TBonusAnual = 1883.63
+            TBonusAnual = 1
             
         elif off == 14:
-            TBonusAnual = 1729.24
+            TBonusAnual = 1
             
         elif off >= 15:
-            TBonusAnual = 1543.97
+            TBonusAnual = 1
             
         
         return (TBonusAnual, TBonusQuarter)
